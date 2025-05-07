dab7d1a95449229dbf2fcbab3eab82acf58ca589
rogerarmandochanac <armandoroger007@gmail.com>
2025-05-07 16:40:56 -0600

    seccion noticias fixed

--- resources/views/snipets/noticias.blade.php
+++ resources/views/snipets/noticias.blade.php
@@ -1,12 +1,12 @@
 <div class="container">
     @if($noticias)
-    <h2 class="noticias__title" id="noticias__titulo">{{$noticias->titulo}}</h2>
     <div class="noticias__container">
         <picture  class="noticias__imagen">
             <img src="{{asset($noticias->image_path)}}" alt="imagen-noticia" id="image__noticia">
         </picture>
+        <h2 class="noticias__title" id="noticias__titulo">{{$noticias->titulo}}</h2><br>
         <div class="noticias__info" id="noticias__contenido">
-            {{$noticias->contenido}}
+            {!! $noticias->contenido !!}
         </div>
     </div>
     @else
