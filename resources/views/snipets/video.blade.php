646e44a4d69adc7bb7405ad1068c73c0a3e3e073
rogerarmandochanac <armandoroger007@gmail.com>
2025-05-27 11:48:07 -0600

    video changed

--- resources/views/snipets/video.blade.php
+++ resources/views/snipets/video.blade.php
@@ -1,3 +1,3 @@
 <video class="video__container" autoplay muted controls loop playsinline>
-    <source src="{{asset('img/porque_invertir.mp4')}}" class="video__video" type=video/mp4>
+    <source src="{{asset('img/sudmedica.mp4')}}" class="video__video" type=video/mp4>
 </video>
