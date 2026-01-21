f6f0bb5e6c32de3bc1f7e808307cbac8e50a5e3b
rogerarmandochanac <armandoroger007@gmail.com>
2026-01-21 15:08:06 -0600

    link to contactos retorno added

--- resources/views/emails/contactanos_retorno.blade.php
+++ resources/views/emails/contactanos_retorno.blade.php
@@ -16,37 +16,37 @@
 <table role="presentation" width="100%" cellspacing="0" cellpadding="0" border="0" style="margin:0; padding:0; border-collapse:collapse;">
   <tr>
     <td style="margin:0; padding:0;">
-      <a href="https://ejemplo.com/00" style="display:block; margin:0; padding:0;">
+      <a href="https://www.sudmedica.com/" style="display:block; margin:0; padding:0;">
         <img src="{{ $message->embed(public_path('img/email_footer_00.png'))}}" 
              width="100%" style="display:block; margin:0; padding:0; border:0; width:100%; height:auto;">
       </a>
     </td>
     <td style="margin:0; padding:0;">
-      <a href="https://ejemplo.com/01" style="display:block; margin:0; padding:0;">
+      <a href="https://www.cmfchile.cl/" style="display:block; margin:0; padding:0;">
         <img src="{{ $message->embed(public_path('img/email_footer_01.png'))}}" 
              width="100%" style="display:block; margin:0; padding:0; border:0; width:100%; height:auto;">
       </a>
     </td>
     <td style="margin:0; padding:0;">
-      <a href="https://ejemplo.com/02" style="display:block; margin:0; padding:0;">
+      <a href="https://cl.linkedin.com/company/sudmedicainversiones?trk=public_post_feed-actor-name" style="display:block; margin:0; padding:0;">
         <img src="{{ $message->embed(public_path('img/email_footer_02.png'))}}" 
              width="100%" style="display:block; margin:0; padding:0; border:0; width:100%; height:auto;">
       </a>
     </td>
     <td style="margin:0; padding:0;">
-      <a href="https://ejemplo.com/03" style="display:block; margin:0; padding:0;">
+      <a href="https://www.instagram.com/sudmedicainversiones/" style="display:block; margin:0; padding:0;">
         <img src="{{ $message->embed(public_path('img/email_footer_03.png'))}}" 
              width="100%" style="display:block; margin:0; padding:0; border:0; width:100%; height:auto;">
       </a>
     </td>
     <td style="margin:0; padding:0;">
-      <a href="https://ejemplo.com/04" style="display:block; margin:0; padding:0;">
+      <a href="https://www.facebook.com/sudmedicainversiones/?_rdr" style="display:block; margin:0; padding:0;">
         <img src="{{ $message->embed(public_path('img/email_footer_04.png'))}}" 
              width="100%" style="display:block; margin:0; padding:0; border:0; width:100%; height:auto;">
       </a>
     </td>
     <td style="margin:0; padding:0;">
-      <a href="https://ejemplo.com/05" style="display:block; margin:0; padding:0;">
+      <a href="https://www.youtube.com/@clinicasudmedica3388" style="display:block; margin:0; padding:0;">
         <img src="{{ $message->embed(public_path('img/email_footer_05.png'))}}" 
              width="100%" style="display:block; margin:0; padding:0; border:0; width:100%; height:auto;">
       </a>
