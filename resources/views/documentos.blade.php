1cd76498e31dc737bdcaea588fd2474caba88662
AzureAD\NilviaAndreaSepúlved <nsepulvedam.srv@chileatiende.cl>
2026-01-30 22:07:47 -0300

    Add documents report pdf 2025

--- resources/views/documentos.blade.php
+++ resources/views/documentos.blade.php
@@ -16,7 +16,13 @@
     @include("snipets.navbar")
     <div class="documentos">
         
-
+      <picture class="documentos__container">
+            <img src="{{asset('img/sudmedica_docs.png')}}" alt="" class="documentos__img">
+            <div class="documentos__texto">
+                <h1 class="documentos__texto__titulo">{{ __('messages.reporte_2025') }}</h1>
+                <a href="{{route('reporte_2025')}}" class="boton__documentos">{{ __('messages.Documentos_Descargar') }}</a>
+            </div>
+        </picture>
      <picture class="documentos__container">
             <img src="{{asset('img/sudmedica_docs.png')}}" alt="" class="documentos__img">
             <div class="documentos__texto">
