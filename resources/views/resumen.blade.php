a482b176957bbc6fcf51a3c381239cf66be7eca9
AzureAD\NilviaAndreaSepúlved <nsepulvedam.srv@chileatiende.cl>
2026-02-19 17:44:26 -0300

    Refactor resumen.blade.php for improved structure and accessibility; update language attribute and enhance download links.

--- resources/views/resumen.blade.php
+++ resources/views/resumen.blade.php
@@ -1,35 +1,66 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{asset('css/style.css')}}">
-    <link rel="stylesheet" href="{{asset('css/popup.css')}}">
-    <link rel="stylesheet" href="{{asset('css/contacto.css')}}">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/3a008cc3c3.js" crossorigin="anonymous"></script>
-    <title>Sudmédica</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/popup.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/contacto.css') }}">
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/3a008cc3c3.js" crossorigin="anonymous"></script>
+
+  <title>Sudmédica</title>
 </head>
-<body>
-    @include("snipets.navbar")
-    <div class="documentos">
-        <picture class="documentos__container">
-            <img src="{{asset('img/sudmedica_docs2.png')}}" alt="" class="documentos__img">
-            <div class="resumen__texto">
-                <div class="resumen_textos">
-                    <h1 class="resumen__texto__uno">{{ __('messages.Primer_semestre_2025') }}</h1>
-                    <h2 class="resumen__texto__dos">{{ __('messages.Resumen_transacciones') }}</h2>
-                </div>
-                <a href="{{route('Reporte_Partes_Relacionadas')}}" class="boton__resumen">{{ __('messages.Documentos_Descargar') }}</a>
-            </div>
-        </picture>
-        
-    </div>
-    @include('snipets.contacto')
-    @include('volver_inicio')
-    <script src="{{asset('js/script.js')}}"></script>
-    <script src="{{asset('js/contacto.js')}}"></script>
+
+<body class="page-resumen-ncg501">
+  @include("snipets.navbar")
+
+  <div class="documentos">
+    <picture class="documentos__container">
+      <img src="{{ asset('img/sudmedica_docs2.png') }}" alt="" class="documentos__img">
+
+      <div class="resumen__texto">
+        <!-- IZQUIERDA -->
+        <div class="resumen_textos">
+          <p class="resumen__tag">RESUMEN NCG501</p>
+
+          <!-- IMPORTANTE:
+               NO agregue "(NCG501)" manualmente acá si su traducción ya lo trae.
+               Deje el título "limpio" y el NCG501 lo muestra el tag.
+          -->
+          <h1 class="resumen__titulo">
+            {{ __('messages.Resumen_transacciones') }}
+          </h1>
+        </div>
+
+        <!-- DERECHA -->
+        <div class="resumen-ncg501__links" aria-label="Descargas NCG501">
+          <div class="resumen-ncg501__row">
+            <span class="resumen-ncg501__periodo">Segundo Semestre 2025</span>
+            <a class="resumen-ncg501__download" href="{{route('reporte_2025')}}">
+              Descargar
+            </a>
+          </div>
+
+          <div class="resumen-ncg501__row">
+            <span class="resumen-ncg501__periodo">Primer Semestre 2025</span>
+            <a class="resumen-ncg501__download" href="{{route('Reporte_Partes_Relacionadas')}}">
+              Descargar
+            </a>
+          </div>
+        </div>
+      </div>
+    </picture>
+  </div>
+
+  @include('snipets.contacto')
+  @include('volver_inicio')
+
+  <script src="{{ asset('js/script.js') }}"></script>
+  <script src="{{ asset('js/contacto.js') }}"></script>
 </body>
 </html>
