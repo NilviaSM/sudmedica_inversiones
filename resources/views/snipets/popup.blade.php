82db7d270aa5e02829cb879dd7ac399891bccb20
AzureAD\NilviaAndreaSepúlved <nsepulvedam.srv@chileatiende.cl>
2026-05-15 17:04:10 -0400

    style: restructure popup component for improved readability and organization

--- resources/views/snipets/popup.blade.php
+++ resources/views/snipets/popup.blade.php
@@ -1,27 +1,42 @@
 <div id="popup_onload" class="popup">
-  <div class="popup-content" role="dialog" aria-modal="true">
-    <div class="popup-body">
+    <div class="popup-content" role="dialog" aria-modal="true">
 
-      <button id="popup_onload_btn_close" class="close" aria-label="Cerrar">&times;</button>
+        <div class="popup-body">
 
-      <img src="{{ asset('img/popup.jpeg') }}" alt="Junta de Accionistas" class="popup-img">
+            <button id="popup_onload_btn_close"
+                    class="close"
+                    aria-label="Cerrar">
+                &times;
+            </button>
 
-      <div class="popup-actions">
-        <a href="{{ route('citacionJOA052026') }}" class="btn btn-overlay">
-          DESCARGAR CITACIÓN
-        </a>
+            <img src="{{ asset('img/popup.jpeg') }}"
+                 alt="Junta de Accionistas"
+                 class="popup-img">
 
-        <a href="{{ route('poderJOA16052026') }}" class="btn btn-overlay-2">
-          DESCARGAR PODER JOA
-        </a>
+           
 
-        <a href="{{ route('documentos') }}" class="btn btn-overlay btn-postulantes">
-          POSTULANTES DIRECTORIO
-        </a>
-      </div>
+            <div class="popup-actions">
+
+                <a href="{{ route('citacionJOA052026') }}"
+                   class="btn btn-overlay">
+                    DESCARGAR CITACIÓN
+                </a>
+
+                <a href="{{ route('poderJOA16052026') }}"
+                   class="btn btn-overlay-2">
+                    DESCARGAR PODER JOA
+                </a>
+
+                <a href="{{ route('documentos') }}"
+                   class="btn btn-overlay btn-postulantes">
+                    POSTULANTES DIRECTORIO
+                </a>
+
+            </div>
+
+        </div>
 
     </div>
-  </div>
 </div>
 <style>
  #popup_onload.popup {
